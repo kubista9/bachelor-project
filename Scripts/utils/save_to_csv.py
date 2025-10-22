a010367bd0da61027672ab51e666e4a31131fbf3
Jakub Kuka <jakubkuka07@gmail.com>
2025-10-22 16:01:25 +0200

    Delete Scripts directory

--- Scripts/utils/save_to_csv.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import os
-
-def save_to_csv(results, category, dir):
-    df = pd.DataFrame(results)
-    os.makedirs(dir, exist_ok=True) # creates directory if it doesn't exist
-    filepath = os.path.join(dir, f"{category}.csv")
-    df.to_csv(filepath, index=False)
-    print(f"✅ Saved {category} data to {filepath}")
